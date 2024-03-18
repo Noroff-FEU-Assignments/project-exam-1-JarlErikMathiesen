dc7e983f07b0f2e3bd3b0dd6b58cd5098133abe1
JarlErikMathiesen <117684143+JarlErikMathiesen@users.noreply.github.com>
2024-03-18 16:35:51 +0100

    Made slider carousel

---
+++ JS/index.js
@@ -0,0 +1,72 @@
+
+const baseUrl = "http://jarlerm.no/wp-json/wp/v2/posts";
+
+
+
+const blogPosts = document.querySelector(".blog-container");
+
+
+
+const rightButton = document.querySelector(".nxt-btn");
+const leftButton = document.querySelector(".pre-btn");
+
+rightButton.addEventListener("click", () => {
+    blogPosts.scrollLeft += 450;
+});
+
+leftButton.addEventListener("click", () => {
+    blogPosts.scrollLeft -= 450;
+});
+
+async function getBlogPosts(url) {
+
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        const posts = json;
+
+        console.log(posts);
+        console.log(posts[0].title.rendered);
+        console.log(posts[0].content.rendered);
+        
+
+
+        blogPosts.innerHTML ="";
+
+        function createHtml(posts){
+            blogPosts.innerHTML = "";
+            posts.forEach(function(post){
+                const postTitle = post.title.rendered;
+                const postContent = post.content.rendered;
+                
+
+                blogPosts.innerHTML += `
+                                        <div class = "blog-card-main">
+                                            <h2>${postTitle}</h2>
+                                                ${postContent}
+                                        </div>`
+                                        
+            })
+
+        }
+
+    createHtml(posts);
+
+
+    }
+
+    catch (error){
+        blogPosts.innerHTML =   `<div class="error">An error occured loading the page</div>`;
+    }
+}
+
+
+getBlogPosts(baseUrl);
+
+
+
+
+/* rightButton.onclick = () => {
+  document.getElementsByClassName("blog-container").scrollLeft += 20;
+};
+ */
